482c86a3e1816fd761287c4035c6117798c57cfa
Dmytro Kulinich <Spo1091@i.ua>
2023-03-18 09:13:21 +0200

    Add homework

---
+++ Asynq and Await/Actions.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Asynq_and_Await
+{
+    public sealed class Actions
+    {
+        public async void Run()
+        {
+            string hello = "Hello";
+            string world = "World";
+            Writer.SaveTextInFile(hello);
+            Writer.SaveTextInFile(world);
+
+            Task<string> firstText = Reader.ReadText(hello);
+            Task<string> secondText = Reader.ReadText(world);
+
+            await Task.WhenAll(firstText, secondText);
+
+            string firstResult = await firstText;
+            string secondResult = await secondText;
+
+            Console.WriteLine(firstResult + " " + secondResult);
+        }
+    }
+}
---
+++ Asynq and Await/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Asynq_and_Await
+{
+    internal class Program
+    {
+         public static void Main(string[] args)
+        {
+            var action = new Actions();
+            action.Run();
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
